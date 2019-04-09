b88312128e0668e0f4acaf5498885434b19d2f4c
ash1969 <nikhil.sonksbs@gmail.com>
2019-04-09 13:10:36 +0530

    Fetching Faculty List // Hard Code Skipped //

--- school/school_app/admin.py
+++ school/school_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(Faculties)
+class FacultyAdmin(admin.ModelAdmin):
+    pass
--- school/school_app/views.py
+++ school/school_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    faculties=Faculties.objects.all()
+    args = {'faculties': faculties, }
+    return render(request, 'index.html', args)
